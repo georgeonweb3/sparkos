fe15b7ee009049daab6ceb0324dcc05b220ccddb
georgeonweb3 <139614108+georgeonweb3@users.noreply.github.com>
2026-05-26 15:11:30 +0100

    Initialize React application in main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SparkOS from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SparkOS />
+  </React.StrictMode>
+)
